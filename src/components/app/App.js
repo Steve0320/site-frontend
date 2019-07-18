3686c12cafe54b07f514cd3bd226a3a8a03b6e82
Steven Bertolucci <srbertol@mtu.edu>
2019-07-17 20:57:20 -0700

    Extract animation code into separate component (RouteAnimator) TODO: Some bugs with transitioning, occasionally an extra component will be added to the DOM during animation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 import {Route, Switch} from "react-router-dom";
-import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 import SplitLayout from "../split-layout/SplitLayout";
 import WorkSiteLayout from "../work-site-layout/WorkSiteLayout";
 import FunSiteLayout from "../fun-site-layout/FunSiteLayout";
+import RouteAnimator from "../route-animator/RouteAnimator"
 
 import styles from './App.module.css';
 
@@ -14,79 +14,48 @@ import styles from './App.module.css';
 
 class App extends React.Component {
 
-  constructor(props) {
-
-    super(props);
-
-    this.state = {
-      animation: {}
-    };
-
-  }
-
-  // componentWillUpdate will be deprecated in React 17, so hook into
-  // componentDidUpdate instead, in accordance with best practices
-  componentDidUpdate(prevProps, prevState, snapshot) {
-
-    let {pathname: path} = prevProps.location;
-    let {pathname: newPath} = this.props.location;
+  getTransition(curPath, newPath) {
 
     // Helper to determine state
     function matchState(targetPath, targetNewPath) {
-      return (path === targetPath && newPath === targetNewPath);
+      return (curPath === targetPath && newPath === targetNewPath);
     }
 
-    // State machine to figure out transitions
-    if (path !== newPath) {
-
-      if (matchState('/', '/work') || matchState('/fun', '/')) {
-        this.setState({animation: {
-            enterActive: styles.pageEnterLeft,
-            exitActive: styles.pageLeaveRight
-          }});
-      }
-
-      else if (matchState('/', '/fun') || matchState('/work', '/')) {
-        this.setState({animation: {
-            enterActive: styles.pageEnterRight,
-            exitActive: styles.pageLeaveLeft
-          }});
-      }
-
-      else {
-        this.setState({animation: {}});
-      }
-
+    if (matchState('/', '/work') || matchState('/fun', '/')) {
+      return {
+        enterActive: styles.pageEnterLeft,
+        exitActive: styles.pageLeaveRight
+      };
+    } else if (matchState('/', '/fun') || matchState('/work', '/')) {
+      return {
+        enterActive: styles.pageEnterRight,
+        exitActive: styles.pageLeaveLeft
+      };
+    } else {
+      return {};
     }
 
   }
 
   /*
-   * Render the top-level UI, animating between states smoothly. Because CSSTransitionGroup
-   * does not update the classNames prop when component is exiting, we use the childFactory prop of
-   * TransitionGroup to dynamically set the classNames when exiting (for a more thorough explanation, see
-   * https://medium.com/lalilo/dynamic-transitions-with-react-router-and-react-transition-group-69ab795815c9)
+   * Render the top-level UI, animating between states smoothly.
    */
   render() {
 
     return (
         <div>
 
-          <TransitionGroup childFactory={child => React.cloneElement(child, {classNames: this.state.animation})}>
-            <CSSTransition key={this.props.location.key} classNames={this.state.animation} timeout={1000}>
-
-              <Switch location={this.props.location}>
-                <Route exact path='/' render={() => <SplitLayout leftLink='/work' rightLink='/fun'/>} />
-                <Route path='/work' component={WorkSiteLayout} />
-                <Route path='/fun' component={FunSiteLayout} />
-                <Route render={() => <div>Not Found</div>} />
-              </Switch>
-
-            </CSSTransition>
-          </TransitionGroup>
+          <RouteAnimator transitionTimeout={1000} getTransition={this.getTransition}>
+            <Switch location={this.props.location}>
+              <Route exact path='/' render={() => <SplitLayout leftLink='/work' rightLink='/fun'/>} />
+              <Route path='/work' component={WorkSiteLayout} />
+              <Route path='/fun' component={FunSiteLayout} />
+              <Route render={() => <div>Not Found</div>} />
+            </Switch>
+          </RouteAnimator>
 
         </div>
-    );
+    )
 
   }
 
---
+++ src/components/route-animator/RouteAnimator.js
@@ -0,0 +1,69 @@
+/* This component builds on the functionality offered by React Transition
+ * Group to allow different transitions when navigating between pages. All
+ * one needs to provide is the timeout value, and the getTransition function,
+ * which should return the set of CSS classes to use for the current page change.
+* */
+
+import React from "react";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
+import {withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class RouteAnimator extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            transitionClasses: {}
+        };
+
+    }
+
+    /* componentWillUpdate will be deprecated in React 17, so hook into componentDidUpdate
+     * instead, in accordance with best practices.
+     */
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+        let {pathname: path} = prevProps.location;
+        let {pathname: newPath} = this.props.location;
+
+        if (path !== newPath) {
+            this.setState({transitionClasses: this.props.getTransition(path, newPath)})
+        }
+
+    }
+
+    /* Wrap the component's children in a CSS transition, using provided props. Because CSSTransitionGroup
+     * does not update the classNames prop when component is exiting, we use the childFactory prop of
+     * TransitionGroup to dynamically set the classNames when exiting (for a more thorough explanation, see
+     * https://medium.com/lalilo/dynamic-transitions-with-react-router-and-react-transition-group-69ab795815c9)
+     */
+    render() {
+
+        return (
+
+            <div>
+
+                <TransitionGroup childFactory={child => React.cloneElement(child, {classNames: this.state.transitionClasses})}>
+                    <CSSTransition key={this.props.location.key} classNames={this.state.transitionClasses} timeout={this.props.transitionTimeout}>
+                        {this.props.children}
+                    </CSSTransition>
+                </TransitionGroup>
+
+            </div>
+
+        );
+
+    }
+
+}
+
+RouteAnimator.propTypes = {
+    transitionTimeout: PropTypes.number,
+    getTransition: PropTypes.func
+};
+
+
+export default withRouter(RouteAnimator);
