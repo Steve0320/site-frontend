0adb9dd6c37aedf92760471fda304e89103e0f5c
Steven Bertolucci <srbertol@mtu.edu>
2019-07-29 21:14:05 -0700

    Clean up setState in CodeProjectLayout Properly name CSS grid areas

--- src/components/code-project-layout/CodeProjectLayout.js
+++ src/components/code-project-layout/CodeProjectLayout.js
@@ -32,48 +32,72 @@ class CodeProjectLayout extends React.Component {
         };
     }
 
+    // Helper method to parse API response data
+    static parseResponse(response) {
+
+        const {data} = response;
+
+        const createdAt = (data.gh_created_at) ? new Date(data.gh_created_at * 1000) : null;
+        const updatedAt = (data.gh_last_updated_at) ? new Date(data.gh_last_updated_at * 1000) : null;
+        const pushedAt = (data.gh_last_pushed_at) ? new Date(data.gh_last_pushed_at * 1000) : null;
+
+        return {
+            name: data.name,
+            shortDescription: data.gh_description,
+            fullDescription: data.description,
+            url: data.gh_url,
+            status: data.status,
+            repoCreatedAt: createdAt,
+            repoLastPushedAt: pushedAt,
+            repoLastUpdatedAt: updatedAt,
+            cloneUrl: data.gh_clone_url,
+            homepage: data.gh_homepage,
+            repoSize: data.gh_size,
+            repoLanguages: data.gh_languages,
+            owner: data.github_user
+        };
+
+    }
+
     // This data does not need to dynamically update, so just load it after component mounts
     componentDidMount() {
         Axios.get('http://localhost:3000/api/v1/projects/' + this.props.projectKey)
             .then(response => {
                 console.log(response);
-                const {data} = response;
-                this.setState({
-                    name: data.name,
-                    shortDescription: data.gh_description,
-                    fullDescription: data.description,
-                    url: data.gh_url,
-                    status: data.status,
-                    repoCreatedAt: new Date(data.gh_created_at * 1000),
-                    repoLastPushedAt: new Date(data.gh_last_pushed_at * 1000),
-                    repoLastUpdatedAt: new Date(data.gh_last_updated_at * 1000),
-                    cloneUrl: data.gh_clone_url,
-                    homepage: data.gh_homepage,
-                    repoSize: data.gh_size,
-                    repoLanguages: data.gh_languages,
-                    owner: data.github_user
-                });
+                const data = CodeProjectLayout.parseResponse(response);
+                data.loading = false;
+                this.setState(data);
             })
             .catch(error => console.log(error));
     }
 
     render() {
 
+        // Only format results if available yet
+        if (this.state.loading) {
+            return (
+                <React.Fragment>
+                    Loading placeholder...
+                </React.Fragment>
+            );
+        }
+
+        // Display data
         return (
-            <div>
-                {this.state.name}<br/>
-                {this.state.shortDescription}<br/>
-                {this.state.fullDescription}<br/>
-                {this.state.url}<br/>
-                {this.state.status}<br/>
-                {(this.state.repoCreatedAt) ? this.state.repoCreatedAt.toLocaleString() : null}<br/>
-                {(this.state.repoLastPushedAt) ? this.state.repoLastPushedAt.toLocaleString() : null}<br/>
-                {(this.state.repoLastUpdatedAt) ? this.state.repoLastUpdatedAt.toLocaleString() : null}<br/>
-                {this.state.cloneUrl}<br/>
-                {this.state.homepage}<br/>
-                {this.state.repoSize}<br/>
-                Languages Placeholder<br/>
-                {this.state.owner}<br/>
+            <div className={styles.container}>
+
+                <div className={styles.header}>
+                    <span className={styles.headerTitle}>{this.state.name}:</span> <i>{this.state.shortDescription}</i>
+                </div>
+
+                <div className={styles.description}>
+                    {this.state.fullDescription}
+                </div>
+
+                <div className={styles.information}>
+                    TBD
+                </div>
+
             </div>
         );
     }
